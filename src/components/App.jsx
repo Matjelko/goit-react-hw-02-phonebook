d786b501ff55eac5717bf57570763962119fd085
Matjelko <matjelko11@gmail.com>
2023-03-24 17:07:58 +0100

    work

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,60 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from "react";
+// import Section from "./Section";
+// import FormName from "./FormName";
+// import Contacts from "./Contacts";
+
+class App extends Component{
+  state = {
+    contacts: [],
+    name: ''
+  }
+
+  // handleChange = (evt) => {
+  //   this.setState({contacts: evt.target.value});
+  // }
+
+  handleSubmit = (evt) => {
+    evt.preventDefault();
+
+    const form = evt.currentTarget;
+    const contact = form.elements.name.value;
+
+    console.log(contact)
+    console.log(this.state)
+
+    form.reset();
+  }
+
+  render(){
+    // const { contacts, name } = this.state;
+
+    return(
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            name="name"
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+          />
+          <button type="submit">Add Contact</button>
+        </form>
+
+        <div>
+
+        </div>
+      </div>
+      // <div>
+      //   {/* <Section title = "Phonebook"> */}
+      //     {/* <FormName onSubmit={values => console.log(values)}/> */}
+      //   {/* </Section> */}
+      //   {/* <Section title = "Contacts"> */}
+      //     {/* <Contacts/> */}
+      //   {/* </Section> */}
+      // </div>
+    )
+  };
+}
+
+export default App;
---
+++ src/components/Contacts.jsx
@@ -0,0 +1,11 @@
+// const Contacts = () => {
+//     return(
+//         <div>
+//             <ul>
+//                 <li></li>
+//             </ul>
+//         </div>
+//     )
+// }
+
+// export default Contacts;
